3693b4589a37d25bb272bdf0a27eb812eeb331bc
danhhuynh25029 <danhhuynh250@gmail.com>
2021-10-11 22:52:39 +0700

    update code

--- project/controllers/views.py
+++ project/controllers/views.py
@@ -1,8 +1,35 @@
 from project import app
-from flask import render_template,request
+from flask import render_template,request,redirect
 from project.models import *
 @app.route('/')
 def start():
     allEmp = databaseConnect.getAllEmp()
-    print(len(allEmp))
+    # print(len(allEmp))
     return render_template('index.html',data=allEmp)
+@app.route("/delete")
+def delete():
+    id = request.args.get("id")
+    databaseConnect.deleteEmp(id)
+    return redirect('/')
+@app.route("/insert",methods=['POST','GET'])
+def insert():
+    if request.method == 'GET':
+        return render_template('insert.html')
+    elif request.method == 'POST':
+        name = request.form['name']
+        phone = request.form['phone']
+        databaseConnect.insertEmp(name, phone)
+        return redirect('/')
+@app.route("/update",methods=['GET','POST'])
+def update():
+    if request.method == 'GET':
+        id = request.args.get("id")
+        # print(id)
+        e = databaseConnect.getEmp(id)
+        return render_template("update.html",data=e)
+    elif request.method == 'POST':
+        id = request.form['id']
+        name = request.form['name']
+        phone = request.form['phone']
+        databaseConnect.updateEmp(id, name, phone)
+        return redirect('/')
--- project/models/databaseConnect.py
+++ project/models/databaseConnect.py
@@ -1,15 +1,23 @@
 import sqlite3
-# from employee import *
 from project.models import employee
 conn = sqlite3.connect("project/models/my_database.db",check_same_thread=False)
 c = conn.cursor()
 # c.execute("INSERT INTO emp(name,phone) VALUES('Danh',12131313)")
+def getEmp(id:int):
+    sql = "SELECT * FROM emp WHERE id={}".format(id)
+    for i in c.execute(sql):
+        e = employee.Employee(i[0],i[1],i[2])
+        return e
 def insertEmp(n:str,p:int):
     sql = "INSERT INTO emp(name,phone) VALUES ('{}',{})".format(n,p)
     c.execute(sql)
     commit_all()
 def deleteEmp(id:int):
     sql = "DELETE FROM emp WHERE id ={}".format(id)
+    c.execute(sql)
+    commit_all()
+def updateEmp(id:int,n:str,p:int):
+    sql = "UPDATE emp SET name='{}',phone={} WHERE id={}".format(n,p,id)
     c.execute(sql)
     commit_all()
 def getAllEmp():
